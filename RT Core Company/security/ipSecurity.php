9bcc672c12d770957cb99cd854daa21425b2b069
Karolina Lewińska <lewinskak95@gmail.com>
2021-01-23 20:34:16 +0100

    Delete RT Core Company directory

--- RT Core Company/security/ipSecurity.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $_SESSION['security'] = hash('ripemd128', $_SERVER['REMOTE_ADDR']
-    . $_SERVER['HTTP_USER_AGENT']);
-
-    if($_SESSION['security'] != hash('ripemd128', $_SERVER['REMOTE_ADDR']
-    . $_SERVER['HTTP_USER_AGENT'])) 
-        header("Location: ../authentication/logout.php");  
-?>
