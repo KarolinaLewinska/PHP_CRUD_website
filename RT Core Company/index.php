2455148c79dfa6af171b40b5f082a2d47b45ffb0
KarolinaLewinska <lewinskak95@gmail.com>
2021-07-08 16:52:24 +0200

    index.php update

--- RT Core Company/index.php
+++ RT Core Company/index.php
@@ -1,7 +1,7 @@
 <?php
     session_start();
 
-    if ((isset($_SESSION['logged_in'])) && ($_SESSION['logged_in']===true)) {
+    if ((isset($_SESSION['logged_in']))) {
         header('Location: employees.php');
         exit();
     }
@@ -12,39 +12,51 @@
       <meta charset="UTF-8">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-      <title>Strona główna</title>
+      <title>RT Core</title>
    </head>
    <body>
-       <br>
        <div>
-            <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top">
-                <a class="navbar-brand" >
-                    <img src="images/logo.jpg" alt="Logo" style="width:60px;">
+            <nav class="navbar navbar-expand-xl bg-dark navbar-dark fixed-top">
+                <a class="navbar-brand">
+                    <div class="row">
+                        <div class="col-sm">
+                            <img src="images/logoNoBackground.png" alt="Logo" style="width:80px;">
+                        </div>
+                        <div class="col-sm">
+                            <h4 style="padding-top:6px;">RT Core Company</h4>
+                        </div>
+                    </div>
                 </a>
                 <ul class="nav navbar-nav ml-auto">
-                    <a>
-                        <img src="images/login.png" alt="login" style="width:40px;">
-                    </a>
-                    <li class="nav-item">
-                        <a class="nav-link" href="authentication/login.php">Logowanie</a>
-                    </li>
-                    <a>
-                        <img src="images/register.png" alt="register" style="width:40px;">
-                    </a>
-                    <li class="nav-item">
-                        <a class="nav-link" href="authentication/register.php">Rejestracja</a>
-                    </li>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-1">
+                                <a><img src="images/login.png" alt="login" style="width:50px; margin-top:5px;"></a>
+                            </div>
+                            <div class="col-4">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="authentication/login.php" style="margin-top:12px; margin-left:15px;">Zaloguj się</a>
+                                </li>
+                            </div> 
+                            <div class="col-1">
+                                <a><img src="images/register.png" alt="register" style="width:50px; margin-top:9px;"></a>
+                            </div>
+                            <div class="col-5">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="authentication/register.php" style="margin-top:12px; margin-left:15px;">Zarejestruj się</a>
+                                </li>
+                            </div>
+                        </div>
+                    </div> 
                 </ul>
             </nav>
        </div>
-       <br><br><br><br>
        <div>
-            <h2 class='text-center'>System zarządzania pracownikami</h2>
+            <h2 class='text-center' style="margin-top: 150px;">System zarządzania danymi pracowników</h2>
         </div>
-        <br>
-        <div>
-            <p class="text-center h6" style="font-size:large;">
-                System służy do zarządzania pracownikami RT Core. 
+        <div style="padding-top:25px;">
+            <p class="text-center" style="font-size:20px;">
+                System umożliwia zarządzania danymi pracowników RT Core. 
                 Do systemu mają dostęp tylko osoby uprawnione. <br>
                 W celu otrzymania uprawnień należy się zalogować.
             </p>
@@ -53,14 +65,12 @@
             <img class ="w-25 p-3 center-block" src="images/logo.jpg" alt="logo"/>
         </div>
         <div>
-            <p class="h2 text-center text-danger bold">
-                RT Core Company
-            </p>
+            <p class="h2 text-center text-danger bold">RT Core Company</p>
         </div>
         <div>
-            <footer class="page-footer  py-2 text-black-50" style="position: fixed; left: 0; bottom: 0; width: 100%; background-color: #B5BEC6">
+            <footer class="page-footer  py-2 text-black-50" style="position:fixed; left:0; bottom:0; width: 100%; background-color:#B5BEC6">
                 <div class="container text-center">
-                    <p>Copyright &copy; RT Core Company 2021 </p>
+                    <p class="font-weight-bold">Copyright &copy; RT Core Company 2021 All Rights Reserved</p>
                 </div>
             </footer>
         </div>
